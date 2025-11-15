223b89907c977c45988e9cbbf6cbf4e9e2d01787
Acoeric98 <98672535+Acoeric98@users.noreply.github.com>
2025-11-15 21:08:04 +0100

    Add files via upload
    mappa files

---
+++ files/external/maintenance.php
@@ -0,0 +1,12 @@
+      <div id="main">
+        <div class="container">
+          <div class="row">
+            <div class="col s12">
+              <div class="card white-text grey darken-4 padding-15">
+                <h4>Server is currently at maintenance.</h4>
+                <p>Thanks for your patience.</p>
+              </div>
+           </div>
+          </div>
+        </div>
+      </div>
